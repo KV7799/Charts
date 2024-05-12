3c265b14097c20e9a58209e421559ca2980a6089
KV7799 <kavindrasingh7799@gmail.com>
2024-05-12 10:24:13 +0530

    modify pie chart

--- piechartofIN/script.js
+++ piechartofIN/script.js
@@ -1,32 +1,32 @@
 const indianPopulationData = [
-    { state: "Andhra Pradesh", population: 53903393 },
-    { state: "Arunachal Pradesh", population: 1570458 },
-    { state: "Assam", population: 35607039 },
-    { state: "Bihar", population: 124799926 },
-    { state: "Chhattisgarh", population: 29436231 },
-    { state: "Goa", population: 1542733 },
-    { state: "Gujarat", population: 63872399 },
-    { state: "Haryana", population: 28204692 },
-    { state: "Himachal Pradesh", population: 7451955 },
-    { state: "Jharkhand", population: 38593948 },
-    { state: "Karnataka", population: 67562686 },
-    { state: "Kerala", population: 35699443 },
-    { state: "Madhya Pradesh", population: 85358965 },
-    { state: "Maharashtra", population: 123144223 },
-    { state: "Manipur", population: 3091545 },
-    { state: "Meghalaya", population: 3366710 },
-    { state: "Mizoram", population: 1239244 },
-    { state: "Nagaland", population: 2249695 },
-    { state: "Odisha", population: 46356334 },
-    { state: "Punjab", population: 30141373 },
-    { state: "Rajasthan", population: 81032689 },
-    { state: "Sikkim", population: 689577 },
-    { state: "Tamil Nadu", population: 77841267 },
-    { state: "Telangana", population: 39362732 },
-    { state: "Tripura", population: 4169794 },
-    { state: "Uttar Pradesh", population: 237882725 },
-    { state: "Uttarakhand", population: 11250858 },
-    { state: "West Bengal", population: 99609303 }
+    { state: "AP", population: 53903393 },
+    { state: "AR", population: 1570458 },
+    { state: "AS", population: 35607039 },
+    { state: "BR", population: 124799926 },
+    { state: "CG", population: 29436231 },
+    { state: "GA", population: 1542733 },
+    { state: "GJ", population: 63872399 },
+    { state: "HR", population: 28204692 },
+    { state: "HP", population: 7451955 },
+    { state: "JH", population: 38593948 },
+    { state: "KA", population: 67562686 },
+    { state: "KL", population: 35699443 },
+    { state: "MP", population: 85358965 },
+    { state: "MH", population: 123144223 },
+    { state: "MN", population: 3091545 },
+    { state: "ML", population: 3366710 },
+    { state: "MZ", population: 1239244 },
+    { state: "NL", population: 2249695 },
+    { state: "OD", population: 46356334 },
+    { state: "PB", population: 30141373 },
+    { state: "RJ", population: 81032689 },
+    { state: "SK", population: 689577 },
+    { state: "TN", population: 77841267 },
+    { state: "TG", population: 39362732 },
+    { state: "TR", population: 4169794 },
+    { state: "UP", population: 237882725 },
+    { state: "UK", population: 11250858 },
+    { state: "WB", population: 99609303 }
 ];
 
 const width = 1000;
@@ -66,7 +66,7 @@ svg.selectAll("path")
     .attr("fill", d => color(d.data.state))
     .attr("d", arc)
     .append("title")
-    .text(d => `${d.data.state}: ${d.data.population.toLocaleString()}`);
+    // .text(d => `${d.data.state}: ${d.data.population.toLocaleString()}`);
 
 
 svg.selectAll("text")
@@ -80,7 +80,7 @@ svg.selectAll("text")
     })
     .attr("dy", "0.35em")
     .attr("text-anchor", d => (d.startAngle + d.endAngle) / 2 < Math.PI ? "start" : "end")
-    .text(d => d.data.population.toLocaleString())
+    .text(d => `${d.data.state}: ${d.data.population.toLocaleString()}`)
     .style("fill", "white")
     .style("font-size", "12px");
 
@@ -92,16 +92,16 @@ const legend = svg.selectAll(".legend")
 
     .attr("transform", (d, i) => `translate(-150,${i * 20})`);
 
-legend.append("rect")
-    .attr("x", width / 1.6)
-    .attr("width", 18)
-    .attr("height", 18)
-    .attr("y", -350)
-    .attr("fill", d => color(d.state));
+// legend.append("rect")
+//     .attr("x", width / 1.6)
+//     .attr("width", 18)
+//     .attr("height", 18)
+//     .attr("y", -350)
+//     .attr("fill", d => color(d.state));
 
-legend.append("text")
-    .attr("x", 620)
-    .attr("y", -343)
-    .attr("dy", ".35em")
-    .style("text-anchor", "end")
-    .text(d => d.state);
+// legend.append("text")
+//     .attr("x", 620)
+//     .attr("y", -343)
+//     .attr("dy", ".35em")
+//     .style("text-anchor", "end")
+    // .text(d => d.state);
